93085a842dce231cb65a6c6f6f5e437ebdf9e0d4
Kai Liu <Kai.Liu@thomsonreuters.com>
2019-07-19 17:26:42 +0800

    FunctionalInterface

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -1,6 +1,17 @@
 package lambdasinaction.chap3;
 
 import java.io.*;
+
+
+/**
+ * 环绕执行： 准备/初始化--->任务--> 清理/结束
+ * 将前后逻辑抽取成模板.
+ *
+ * 1.行为参数化： (BufferedReader b) -> b.readLine()
+ * 2.使用函数接口传递行为：接口参数，返回值对应 1. 然后将接口作为方法的一个参数
+ * 3.执行行为，实现上个方法的内容（处理接口的参数。。）
+ * 4.传递 Lambda
+ */
 public class ExecuteAround {
 
 	public static void main(String ...args) throws IOException{
@@ -11,7 +22,7 @@ public class ExecuteAround {
 
         System.out.println("---");
 
-		String oneLine = processFile((BufferedReader b) -> b.readLine());
+		String oneLine = processFile(b -> b.readLine());
 		System.out.println(oneLine);
 
 		String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
@@ -21,12 +32,19 @@ public class ExecuteAround {
 
     public static String processFileLimited() throws IOException {
         try (BufferedReader br =
-                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+                     new BufferedReader(new FileReader("lambdasinaction//chap3//data.txt"))) {
+            // 在这个方法中就是 环绕执行(execute around)
             return br.readLine();
         }
     }
 
 
+    /**
+     * 行为参数化---> lambda
+     * @param p
+     * @return
+     * @throws IOException
+     */
 	public static String processFile(BufferedReaderProcessor p) throws IOException {
 		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
 			return p.process(br);
@@ -35,7 +53,6 @@ public class ExecuteAround {
 	}
 
 	public interface BufferedReaderProcessor{
-		public String process(BufferedReader b) throws IOException;
-
+		String process(BufferedReader b) throws IOException;
 	}
 }
--- src/main/java/lambdasinaction/chap3/Sorting.java
+++ src/main/java/lambdasinaction/chap3/Sorting.java
@@ -1,7 +1,11 @@
 package lambdasinaction.chap3;
 
-import java.util.*;
 import static java.util.Comparator.comparing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Sorting {
 
@@ -9,7 +13,7 @@ public class Sorting {
 
         // 1
         List<Apple> inventory = new ArrayList<>();
-        inventory.addAll(Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red")));
+        inventory.addAll(Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red")));
 
         // [Apple{color='green', weight=80}, Apple{color='red', weight=120}, Apple{color='green', weight=155}]
         inventory.sort(new AppleComparator());
